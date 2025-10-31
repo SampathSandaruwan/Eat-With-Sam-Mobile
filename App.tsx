7f404fb6cccad648e1d9952ecdd549506fc81e33
Sampath Sandaruwan <sampath@echonlabs.com>
2025-10-31 21:32:23 +0530

    chore(deps): :heavy_plus_sign: add react-native dependencies and configure iOS simulator

--- App.tsx
+++ App.tsx
@@ -5,12 +5,12 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { NewAppScreen } from '@react-native/new-app-screen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
